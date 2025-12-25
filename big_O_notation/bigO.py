d288b396f79a19ca62a192816db63b09ef9bc8fb
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-25 23:25:21 +0600

    auto commit Thu Dec 25 23:25:21 +06 2025

--- big_O_notation/bigO.py
+++ big_O_notation/bigO.py
@@ -1,7 +1,7 @@
 arr = [7, 4, 9, 5, 3, 8, 2, 1]
 print("cheking", arr)
-for i in range(len(arr)):
-    for j in range(len(arr)):
-        if arr[j] > arr[j + 1]:
-            arr[j], arr[j + 1] = arr[j + 1], arr[j]
-print("enhanced arr", arr)
+   for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    print("enhanced arr", arr)
