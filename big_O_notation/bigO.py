d44e04cbe01f5b9d3547ced141c8e1c2c69638f1
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-31 00:23:03 +0600

    auto commit Wed Dec 31 00:23:03 +06 2025

--- big_O_notation/bigO.py
+++ big_O_notation/bigO.py
@@ -72,17 +72,18 @@
 # ---binary search---------------
 
 
-# def binary_search(arr, target):
-#     left, right = 0, len(arr) - 1
-#     while left <= right:
-#         mid = (left + right) // 2
-#         if arr[mid] == target:
-#             return mid
-#         elif arr[mid] < target:
-#             left = mid + 1
-#         else:
-#             right = mid - 1
-#     return -1
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
 
 # example & pattern of useage
 arr = [1, 3, 5, 7, 9, 11, 13]
