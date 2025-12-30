1bac6a9aa5b1244cd1ba41e3bc158e1364dbd81d
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-31 00:24:09 +0600

    auto commit Wed Dec 31 00:24:09 +06 2025

--- big_O_notation/bigO.py
+++ big_O_notation/bigO.py
@@ -48,9 +48,9 @@
 
 
 #  O(logN)
-# arr = [1, 2, 4, 5, 6, 7, 9]
-# index = bisect.bisect_left(arr, 5)
-# print(index)  # 3
+arr = [1, 2, 4, 5, 6, 7, 9]
+index = bisect.bisect_left(arr, 5)
+print(index)  # 3
 
 # -------------
 
