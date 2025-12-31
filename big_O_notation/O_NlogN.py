c5807f9dac93d6b90f8b6efd88aa8e4f215a006e
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-31 23:39:02 +0600

    auto commit Wed Dec 31 23:39:02 +06 2025

--- big_O_notation/O_NlogN.py
+++ big_O_notation/O_NlogN.py
@@ -1,18 +1,20 @@
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
-# divide (log N) linear way
-    mid = len(arr)//2
-    left = arr[:mid]
-    right = arr[mid:]
-# via recursion sort both sides
-    left = merge_sort(left)
-    right = merge_sort(right)
-# now act likely O(n)
-    return merge_sort(left, right)
 
 
 a = merge_sort(arr=[1, 4, 9, 5, 3, 8, 2, 1])
+print(a)
+
+# divide (log N) linear way
+mid = len(arr)//2
+left = arr[:mid]
+right = arr[mid:]
+# via recursion sort both sides
+left = merge_sort(left)
+right = merge_sort(right)
+# now act likely O(n)
+return merge_sort(left, right)
 
 
 # how merge() fn works
