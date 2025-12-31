a230b001c1931c4a0d1a2f39a8986707aa644f58
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-31 23:40:08 +0600

    auto commit Wed Dec 31 23:40:08 +06 2025

--- big_O_notation/O_NlogN.py
+++ big_O_notation/O_NlogN.py
@@ -7,9 +7,11 @@ def merge_sort(arr):
 mid = len(arr)//2
 left = arr[:mid]
 right = arr[mid:]
+
 # via recursion sort both sides
 left = merge_sort(left)
 right = merge_sort(right)
+
 # now act likely O(n)
 return merge_sort(left, right)
 
