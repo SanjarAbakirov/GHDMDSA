cfaece7a024a3e56da8239539f6fa78fe5dee6cd
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-02 23:34:42 +0600

    auto commit Fri Jan  2 23:34:42 +06 2026

--- big_O_notation/O_NlogN.py
+++ big_O_notation/O_NlogN.py
@@ -72,10 +72,6 @@ def sort_and_merge(arr):
     return sort_and_merge(left, right)  # sort_and_merge([45], [8])
 
 
-a = sort_and_merge(left, right)
-print(a)
-
-
 def merge_arr(left, right):
     a = 0
     result = []
