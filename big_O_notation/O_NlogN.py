695a1527fc76189311544ae749dcb03802d0c6da
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-14 19:59:09 +0600

    auto commit Wed Jan 14 19:59:09 +06 2026

--- big_O_notation/O_NlogN.py
+++ big_O_notation/O_NlogN.py
@@ -193,3 +193,20 @@ def bubble_sort(arr):
 numbers = [64, 34, 25, 12, 22, 11, 90]
 print("before sorting", numbers)
 print("after sorting", bubble_sort(numbers.copy()))
+
+# -----------------square complexity--------
+
+
+def selection_sort(arr):
+    "at each staep seeking min"
+    n = len(arr)
+    for i in range(n):
+        min_index = i  # preserve this value is minimum at this position
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+numbers = [64, ]
