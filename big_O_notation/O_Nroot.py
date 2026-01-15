3130d83b376ae4bd6d4be6ec8c93cac76004283e
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-15 22:49:21 +0600

    auto commit Thu Jan 15 22:49:21 +06 2026

---
+++ big_O_notation/O_Nroot.py
@@ -0,0 +1,45 @@
+# ------------------root complexity-----
+# поиск всех делителей с коренвой сложностью
+
+
+def find_divisions(n):
+    # O(root n complexity)
+    if n <= 0:
+        return []
+
+    divisors = []
+    i = 1
+
+    # iteration only from square num from n
+    while i * i <= n:
+        if n % i == 0:
+            divisors.append(i)
+# if divisors is not square root adding pare divisor
+        i += 1
+
+#  sort results
+    divisors.sort()
+    return divisors
+
+
+# -----------
+def frind_divisors_optimized(n):
+    # math.isqrt()
+    if n <= 0:
+        return []
+
+    divisors_1 = []
+    sqrt_n = math.isqrt(n)
+
+    for i in range(1, sqrt_n + 1):
+        if n % i == 0:
+            divisors_1.append(i)
+            if i != n // i:
+                divisors_1.append(n // i)
+
+    divisors_1.sort()
+    return divisors_1
+
+
+x = frind_divisors_optimized(49)
+# print(x)
