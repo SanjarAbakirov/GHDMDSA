85110d8e274065e88d6f33b58e807260bf81b28a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-15 22:57:16 +0600

    auto commit Thu Jan 15 22:57:16 +06 2026

--- big_O_notation/O_Nconstant.py
+++ big_O_notation/O_Nconstant.py
@@ -20,3 +20,28 @@ print(students_ids)
 goods = {"shues", "t-shorts", "throusers", "caps", "skirts"}
 goods_ids = {id: item for id, item in enumerate(goods)}
 print(goods_ids)
+
+# ------------------
+# O(1) constant & complicate
+# my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# first_element = my_list[0]
+# middle_element = my_list[5]
+# last_element = my_list[-1]
+# print(first_element)
+# print(middle_element)
+# print(last_element)
+
+# -----------------
+# my_list = []
+# my_list.append(1)
+# my_list.append(2)
+# my_list.append(3)
+# print(my_list)
+
+# last_element = my_list.pop()
+# print(my_list)
+
+# my_list.insert(0, 0)
+# my_list.pop(0)
+# print(my_list)
+# ---------------
