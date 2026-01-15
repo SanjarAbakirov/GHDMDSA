beecb08b8553fe3694f365c954dd37622a077160
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-15 23:05:54 +0600

    auto commit Thu Jan 15 23:05:54 +06 2026

--- big_O_notation/O_Nsqare.py
+++ big_O_notation/O_Nsqare.py
@@ -36,21 +36,21 @@ numbers = [64, 34, 25, 12, 22, 11, 90]
 # ----- square complexity -------
 
 
-def find_all_divisors(n: int) -> list:
-    """Нахождение всех делителей числа за O(√n)"""
-    divisors = []
+# def find_all_divisors(n: int) -> list:
+#     """Нахождение всех делителей числа за O(√n)"""
+#     divisors = []
 
-    # Идем только до √n
-    for i in range(1, int(math.isqrt(n)) + 1):  # for i in range(1, 7)
-        if n % i == 0:
-            divisors.append(i)
-            # Добавляем парный делитель (кроме случая i = n/i)
-            if i != n // i:
-                divisors.append(n // i)
+#     # Идем только до √n
+#     for i in range(1, int(math.isqrt(n)) + 1):  # for i in range(1, 7)
+#         if n % i == 0:
+#             divisors.append(i)
+#             # Добавляем парный делитель (кроме случая i = n/i)
+#             if i != n // i:
+#                 divisors.append(n // i)
 
-    return sorted(divisors)
+#     return sorted(divisors)
 
 
-# Пример
-print(find_all_divisors(36))  # [1, 2, 3, 4, 6, 9, 12, 18, 36]
-print(find_all_divisors(17))  # [1, 17]
+# # Пример
+# print(find_all_divisors(36))  # [1, 2, 3, 4, 6, 9, 12, 18, 36]
+# print(find_all_divisors(17))  # [1, 17]
