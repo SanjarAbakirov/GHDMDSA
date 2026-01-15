3383ed6a916fae0d6db3b92304bb1c17b8096cf3
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-15 22:59:10 +0600

    auto commit Thu Jan 15 22:59:10 +06 2026

--- big_O_notation/bigO.py
+++ big_O_notation/bigO.py
@@ -6,30 +6,6 @@
 #             arr[j], arr[j + 1] = arr[j + 1], arr[j]
 # print("enhanced arr", arr)
 
-# ------------------
-# O(1) constant & complicate
-# my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-# first_element = my_list[0]
-# middle_element = my_list[5]
-# last_element = my_list[-1]
-# print(first_element)
-# print(middle_element)
-# print(last_element)
-
-# -----------------
-# my_list = []
-# my_list.append(1)
-# my_list.append(2)
-# my_list.append(3)
-# print(my_list)
-
-# last_element = my_list.pop()
-# print(my_list)
-
-# my_list.insert(0, 0)
-# my_list.pop(0)
-# print(my_list)
-# ---------------
 
 import math
 import bisect
