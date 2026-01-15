185caf1e05d5470304350657e1b86a2b219c8a80
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-15 22:55:12 +0600

    auto commit Thu Jan 15 22:55:12 +06 2026

--- big_O_notation/O_Nconstant.py
+++ big_O_notation/O_Nconstant.py
@@ -7,13 +7,13 @@ for i in range(len(arr)):
     dict_from_list1[i] = arr[i]
 
 result1 = dict_from_list1
-# print(result1)
-# print(dict_from_list)
+print(result1)
+print(dict_from_list)
 
 # ----O(1)----
 students = ["ann", "boris", "victor", "galina"]
 students_ids = {id: name for id, name in enumerate(students, start=100)}
-# print(students_ids)
+print(students_ids)
 
 # ------O(1)----
 goods = {"shues", "t-shorts", "throusers", "caps", "skirts"}
