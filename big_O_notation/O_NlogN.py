65d1ff283f3915217c9aa93a96b0f0428df82e05
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-15 22:56:30 +0600

    auto commit Thu Jan 15 22:56:30 +06 2026

--- big_O_notation/O_NlogN.py
+++ big_O_notation/O_NlogN.py
@@ -39,27 +39,6 @@ def merge(left, right):
     # print(result)
 
 
-# --------------O(1) constant--------
-dict_from_list = {i: val for i, val in enumerate(arr)}
-
-dict_from_list1 = {}
-for i in range(len(arr)):
-    dict_from_list1[i] = arr[i]
-
-result1 = dict_from_list1
-# print(result1)
-# print(dict_from_list)
-
-# ----O(1)----
-students = ["ann", "boris", "victor", "galina"]
-students_ids = {id: name for id, name in enumerate(students, start=100)}
-# print(students_ids)
-
-# ------O(1)----
-goods = {"shues", "t-shorts", "throusers", "caps", "skirts"}
-goods_ids = {id: item for id, item in enumerate(goods)}
-# print(goods_ids)
-
 # -----O(N log N)-------
 arr = [45, 8, 91, 87, 1, 4, 2, 9, 7, 0, 3, 6]
 
