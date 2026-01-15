a7c5841744e7d008fd25955875731129d055cb02
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-15 23:10:11 +0600

    auto commit Thu Jan 15 23:10:11 +06 2026

--- big_O_notation/O_Ncube.py
+++ big_O_notation/O_Ncube.py
@@ -4,13 +4,13 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-    n = len(nums)
-    result = []
-    for i in range(n - 2):
-        for j in range(i + 1, n - 1):
-            for k in range(j + 1, n):
-                if nums[i] + nums[j] + nums[k] == 0:
-                    triplet = sorted([nums[i], nums[j], nums[k]])
-                    if triplet not in result:
-                        result.append(triplet)
-    return result
+        n = len(nums)
+        result = []
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                for k in range(j + 1, n):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        triplet = sorted([nums[i], nums[j], nums[k]])
+                        if triplet not in result:
+                            result.append(triplet)
+        return result
