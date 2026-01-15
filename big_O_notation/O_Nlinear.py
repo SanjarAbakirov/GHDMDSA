5ad5f69b996921b685ae7b6ee7faf539db0bb504
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-16 00:01:51 +0600

    auto commit Fri Jan 16 00:01:51 +06 2026

--- big_O_notation/O_Nlinear.py
+++ big_O_notation/O_Nlinear.py
@@ -18,3 +18,8 @@ a = binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 7)
 print(a)
 
 # -----------
+
+
+arr = ["A", "B", "C"]
+dict_from_list2 = {i: val for i, val in enumerate(arr)}
+# print(dict_from_list2)
