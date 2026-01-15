ca250d111b5c64cd101930247e19388dead86257
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-15 22:46:55 +0600

    auto commit Thu Jan 15 22:46:55 +06 2026

--- big_O_notation/O_NlogN.py
+++ big_O_notation/O_NlogN.py
@@ -212,19 +212,3 @@ def selection_sort(arr):
 numbers = [64, 55, 82, 34, 25, 12, 22, 11, 90, 73]
 # print("before sorting", numbers)
 # print("after sorting", selection_sort(numbers.copy()))
-
-
-# -------cube complexity----------------
-
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[list[List[int]]:
-    n = len(nums)
-    result = []
-    for i in range(n - 2):
-        for j in range(i + 1, n - 1):
-            for k in range(j + 1, n):
-                if nums[i] + nums[j] + nums[k] == 0:
-                    triplet = sorted([nums[i], nums[j], nums[k]])
-                    if triplet not in result:
-                        result.append(triplet)
-    return result
