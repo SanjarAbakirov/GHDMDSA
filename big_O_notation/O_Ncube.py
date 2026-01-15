b5c8d1caec4c9990b8b50fc06ee7d55d7a7f0a68
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-15 23:21:12 +0600

    auto commit Thu Jan 15 23:21:12 +06 2026

--- big_O_notation/O_Ncube.py
+++ big_O_notation/O_Ncube.py
@@ -14,6 +14,7 @@ class Solution:  # declaring the class
         result = []
         print(f"Input list: {nums}")
         print(f"Length: {n}")
+
         for i in range(n - 2):
             for j in range(i + 1, n - 1):
                 for k in range(j + 1, n):
