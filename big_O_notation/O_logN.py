6ef6935640af2f2eac4641ea805198eea0ade519
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-15 23:00:19 +0600

    auto commit Thu Jan 15 23:00:19 +06 2026

--- big_O_notation/O_logN.py
+++ big_O_notation/O_logN.py
@@ -1,4 +1,5 @@
-
+import math
+import bisect
 #  O(logN)
 arr = [1, 2, 4, 5, 6, 7, 9]
 index = bisect.bisect_left(arr, 5)
--- big_O_notation/bigO.py
+++ big_O_notation/bigO.py
@@ -8,7 +8,7 @@
 
 
 import math
-import bisect
+
 my_dict = {'apple': 5, 'banana': 10, 'orange': 7}
 my_set = {1, 2, 3, 4, 5}
 # O(1) in average case
@@ -23,11 +23,6 @@ exists = 3 in my_set
 # print(my_set)
 # print(my_dict)
 
-
-#  O(logN)
-arr = [1, 2, 4, 5, 6, 7, 9]
-index = bisect.bisect_left(arr, 5)
-# print(index)  # 3
 
 # -------------
 
